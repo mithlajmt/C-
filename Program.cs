b9a85ce82ab4b07967b382e556789e789b0143fa
mithlajmatta <144442116+mithlajmatta@users.noreply.github.com>
2024-07-16 18:26:01 +0530

    numeric data types

--- Program.cs
+++ Program.cs
@@ -11,10 +11,24 @@ namespace firstapp
         static void Main(string[] args)
         {
             Console.WriteLine("hey comeon");
+            int x=2,y=3;
             int age = 10;
-            long num = 4545458924983249294;
-            Console.WriteLine(num);
+            long num = 4545458924983249294L;
+            double fr = 10.3D;
+            float floatin = 13.253400F;
+            decimal money = 232323M;
+
+            age = 89;
+
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
             Console.WriteLine(age);
+            Console.WriteLine(num);
+            Console.WriteLine(fr);
+            Console.WriteLine(floatin);
+            Console.WriteLine(money);
             Console.ReadLine();
 
         }
