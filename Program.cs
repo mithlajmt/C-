9c0cb0083a8b1c3d6691902e5b519f86e3ac6006
mithlajmatta <144442116+mithlajmatta@users.noreply.github.com>
2024-07-16 18:00:33 +0530

    hello world

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hey comeon");
+            int age = 10;
+            long num = 4545458924983249294;
+            Console.WriteLine(num);
+            Console.WriteLine(age);
+            Console.ReadLine();
+
+        }
+    }
+}
